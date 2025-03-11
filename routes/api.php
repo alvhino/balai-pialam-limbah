989cf8ff336e2089cc54928ed707258b6c398596
remasnarmusaba <remasnarmsb@gmail.com>
2025-03-11 13:46:29 +0700

    memperbaiki code pada api.php

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\TrukController;
+use App\Http\Controllers\LoginController;
 
 
 Route::get('/user', function (Request $request) {
@@ -13,5 +14,11 @@ Route::get('/user', function (Request $request) {
 // route untuk user
 Route::get('/user/search', [UserController::class, 'search']);
 Route::resource('user', UserController::class);
+
 // route untuk truk
 Route::resource('truk', TrukController::class);
+
+// Rute login dan logout
+Route::get('login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('login', [LoginController::class, 'login'])->name('login.proses')->middleware('guest');
+Route::get('logout', [LoginController::class, 'logout'])->name('logout');
